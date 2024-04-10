72c3fe99e23f3aac1def0c640effd30c3ef76bf7
warfielda01 <93686476+warfielda01@users.noreply.github.com>
2024-04-10 10:39:24 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 # Function to generate a random password
 generate_password() {
     tr -dc A-Za-z0-9 </dev/urandom | head -c 15 ; echo ''
@@ -8,9 +14,10 @@ generate_password() {
 # Function to install required packages from requirements.txt
 install_requirements() {
     if [ -f "requirements.txt" ]; then
+        echo -e "${YELLOW}Installing required packages...${NC}"
         sudo apt install $(cat requirements.txt)
     else
-        echo "Error: requirements.txt not found."
+        echo -e "${RED}Error: requirements.txt not found.${NC}"
         exit 1
     fi
 }
@@ -18,10 +25,10 @@ install_requirements() {
 # Function to replace or create a file with a provided one
 replace_or_create_file() {
     if [ -f "$2" ]; then
-        echo "Replacing $2 with provided file..."
+        echo -e "${YELLOW}Replacing $2 with provided file...${NC}"
         sudo cp "$1" "$2"
     else
-        echo "Creating $2 with provided file..."
+        echo -e "${YELLOW}Creating $2 with provided file...${NC}"
         sudo cp "$1" "$2"
     fi
 }
@@ -29,23 +36,24 @@ replace_or_create_file() {
 # Function to make a script executable
 make_executable() {
     if [ -f "$1" ]; then
-        echo "Making $1 executable..."
+        echo -e "${YELLOW}Making $1 executable...${NC}"
         sudo chmod +x "$1"
     else
-        echo "Error: $1 not found."
+        echo -e "${RED}Error: $1 not found.${NC}"
         exit 1
     fi
 }
 
 # Function to create /etc/snmp/snmpd.conf
 create_snmpd_conf() {
-    echo "Creating /etc/snmp/snmpd.conf..."
-    echo "Please enter the sysLocation:"
+    echo -e "${YELLOW}Creating /etc/snmp/snmpd.conf...${NC}"
+    echo -e "${YELLOW}Please enter the sysLocation:${NC}"
     read sysLocation
-    echo "Please enter the email:"
+    echo -e "${YELLOW}Please enter the email:${NC}"
     read sysContact
+
     # Set the system name (hostname)
-    echo "Please enter the computer name:"
+    echo -e "${YELLOW}Please enter the computer name:${NC}"
     read computerName
     sudo hostnamectl set-hostname "$computerName"
 
@@ -63,38 +71,39 @@ rouser authPrivUser authpriv -V systemonly
 includeDir /etc/snmp/snmp.conf.d
 EOT
 
-    # Add extend directive for ups-status.sh - dont know this is needed
-    #echo "Adding extend directive for ups-status.sh..."
+    # Move snmpd.conf to /etc/snmp directory
+    sudo mv "config/snmpd.conf" "/etc/snmp/" || { echo -e "${RED}Failed to move snmpd.conf to /etc/snmp/${NC}"; exit 1; }
+
+    # Add extend directive for ups-status.sh
+    #echo -e "${YELLOW}Adding extend directive for ups-status.sh...${NC}"
     #sudo upsc ups@localhost | sed 's/^\(.*\): .*$/extend \1 \/usr\/local\/bin\/ups-status.sh \1/' >> "/etc/snmp/snmpd.conf"
 }
 
 # Function to start and enable nut-server service
 start_and_enable_services() {
-    echo "Starting nut-server service..."
+    echo -e "${YELLOW}Starting nut-server service...${NC}"
     sudo service nut-server start
     sudo systemctl enable nut-server
-    echo "Starting snmpd service..."
+
+    echo -e "${YELLOW}Starting snmpd service...${NC}"
     sudo service snmpd start
     sudo systemctl enable snmpd
 
-    echo "Enabling nut-server and snmpd services to start on boot..."
-    #sudo systemctl enable nut-server.service
-    #sudo systemctl enable snmpd.service
+    echo -e "${GREEN}Services enabled to start on boot.${NC}"
 }
 
 # Create a user called localadmin with a random password
-echo "Creating user 'localadmin'..."
+echo -e "${YELLOW}Creating user 'localadmin'...${NC}"
 password=$(generate_password)
 sudo useradd -m -s /bin/bash localadmin
-echo "Setting password for 'localadmin' to: $password"
+echo -e "${YELLOW}Setting password for 'localadmin' to: $password${NC}"
 echo "localadmin:$password" | sudo chpasswd
 
 # Add 'localadmin' to the sudo group
-echo "Adding 'localadmin' to the sudo group..."
+echo -e "${YELLOW}Adding 'localadmin' to the sudo group...${NC}"
 sudo usermod -aG sudo localadmin
 
 # Install required services from requirements.txt
-echo "Installing required services..."
 install_requirements
 
 # Create /etc/snmp/snmpd.conf and add extend directive
@@ -120,4 +129,5 @@ push_ups_status
 # Start and enable nut-server service
 start_and_enable_services
 
-echo "Setup completed successfully."
+echo -e "${GREEN}Setup completed successfully.${NC}"
+
